6e941de8c02bab93408e2ab93cd7a6564b92a466
Chetan <kaushikchetan0003@gmail.com>
2025-06-08 15:06:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  const hamburger = document.getElementById("hamburger");
+  const navItems = document.getElementById("nav-items");
+
+  hamburger.addEventListener("click", () => {
+    navItems.classList.toggle("active");
+  });
